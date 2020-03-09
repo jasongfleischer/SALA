4756c60af3e1b4981f9bce641a513338bd5f468c
Jason Fleischer <jason.g.fleischer@gmail.com>
2020-03-09 13:05:16 -0700

    everything in Analyze by person is done now; this framework is also the one to follow for future work; everything is python 3 compliant in load/firsttime/analyze; things use parquet to save raw/timing data to maintain the data types as much as possible which doesnt work with csv

--- firsttime.py
+++ firsttime.py
@@ -17,27 +17,30 @@ def firstAndLastLight(data, threshold_list, resamp=False):
     lastlight = []
     min2fl = []
     min2ll = []
-    timabove = []
     whoswatch = []
     watchperiod = []
     thresholds = []
     datelist = []
     grouplist = []
+    totalact=[]
+    tabvlight=[]
+    tabvlightAM=[]
+    tluxmin = []
+    tluxminAM = []
 
-    for id in ids:
-            print(id)
-            these_rows = (data.UID == id) & (data['Interval Status'].isin(['ACTIVE','REST'])) & np.logical_not(data['Off-Wrist Status'])
+    for uid in ids:
+            print(uid)
+            these_rows = (data.UID == uid) & (data['Interval Status'].isin(['ACTIVE','REST'])) & np.logical_not(data['Off-Wrist Status'])
             
-            if these_rows.sum() == 0:
-                print("OOOPS!!#!#! "+id+" has no ACTIVE rows")
+            assert (these_rows.sum() > 0),"OOOPS!!#!#! "+uid+" has no ACTIVE rows"
                 
                 
             daysofdata = set( data[ these_rows ].index.date )
             
             if 'Group' in data.columns:
-                group = data[data.UID == id].iloc[0,:].Group
+                group = data[data.UID == uid].iloc[0,:].Group
             elif 'Season' in data.columns:
-                group = data[data.UID == id].iloc[0,:].Season
+                group = data[data.UID == uid].iloc[0,:].Season
             else:
                 print("OOOPS!!#!  No group variable??")
                 raise ValueError
@@ -46,27 +49,32 @@ def firstAndLastLight(data, threshold_list, resamp=False):
                 nextday = a_day + pd.tseries.offsets.Day()
                 nextday = nextday.date().isoformat()
                 thisday = a_day.isoformat()
-                daylight = data['White Light'][these_rows][thisday + ' 04:00:00' : nextday + ' 03:59:00']
-                datelist.append(a_day)
-                grouplist.append(group)
-                if resamp:
+                daylight = data[these_rows][thisday + ' 04:00:00' : nextday + ' 03:59:00']['White Light']
+                if resamp: # resample if the function argument is set
                     daylight = daylight.resample(resamp[1]).apply(resamp[0]) 
+                
+                # watch update period for todays data
+                dperiod = daylight.index.to_series().diff().min() 
+                dpmult = dperiod/pd.Timedelta('1 min') # multiplier to get lux-minutes later
+                
+                lxmin =  dpmult * daylight.sum()
+                lxminAM = dpmult * daylight[:thisday + ' 12:00'].sum()
+                
                 for a_thresh in threshold_list:                                  
                     thresholds.append(a_thresh)
                     if a_thresh==0:
-                        abovethresh = daylight.index[ daylight < a_thresh+3]
+                        abovethresh = daylight.index[ daylight < 5] # 0 theshold is a request to calculate under 5 lux
+                        abovethreshAM = daylight[:thisday + ' 12:00'].index[ daylight[:thisday + ' 12:00'] < 5]
                     else:
                         abovethresh = daylight.index[ daylight > a_thresh]
-                    if len(daylight)>15: # let's make sure we have at least 15min of daytime to work with
-                        # sometimes the index skips a few minutes here and there... this might be due to the Interval Status
-                        # so we need a way to find the true period in this noisy data...  
-                        dperiod = pd.Timedelta(np.min(daylight.index[1:15]-daylight.index[0:14])) # this is the update rate of the watch
-                        timeabove = len(abovethresh) * dperiod 
-                    else:
-                        dperiod = 0
-                        timeabove = pd.Timedelta('0 seconds')                        
-                    timabove.append(timeabove)
+                        abovethreshAM = daylight[:thisday + ' 12:00'].index[ daylight[:thisday + ' 12:00'] > a_thresh]         
+                    tabvlight.append( dperiod * len(abovethresh))
+                    tabvlightAM.append( dperiod * len(abovethreshAM))
+                    tluxmin.append( lxmin )
+                    tluxminAM.append( lxminAM )
                     watchperiod.append(dperiod)
+                    datelist.append(a_day)
+                    grouplist.append(group)
                     try:
                         timelight = abovethresh[-1] # last time is above threshold
                         mins4am = (timelight.time().hour - 4) * 60 + timelight.time().minute
@@ -87,126 +95,17 @@ def firstAndLastLight(data, threshold_list, resamp=False):
                         mins4am = np.nan
                     firstlight.append(timelight)
                     min2fl.append(mins4am)
-                    whoswatch.append(id)
-                    #print("{} {} {} of 0-lux with period {}\n".format(id,a_day,len(daylight[daylight==0])*dperiod,dperiod))
+                    whoswatch.append(uid)
+                    #print("{} {} {} of 0-lux with period {}\n".format(uid,a_day,len(daylight[daylight==0])*dperiod,dperiod))
                 
                 #print( len( whoswatch), len(lastlight), len(firstlight), len(min2ll), len(min2fl))
     return pd.DataFrame( {'UID': whoswatch, 'Date': datelist, 'Threshold': thresholds,
                           'Last Light': lastlight, 'Mins to LL from 4AM': min2ll,
                           'First Light': firstlight, 'Mins to FL from 4AM': min2fl,
-                          'Time above threshold': timabove, 
-                          'Minutes above threshold': [ el.total_seconds()/60.0 for el in timabove],
+                          'Time above threshold': tabvlight, 'Time above threshold AM': tabvlightAM,
+                          'Minutes above threshold': [ el.total_seconds()/60.0 for el in tabvlight],
+                          'Minutes above threshold AM': [ el.total_seconds()/60.0 for el in tabvlightAM],
+                          'Lux minutes': tluxmin, 'Lux minutes AM': tluxminAM,
                           'Group': grouplist,
                           'Watch period': watchperiod 
                          } )
-#%%
-
-def add_astral_data(timingData):
-
-    
-    a = Astral()
-    tobaloc = Location(('Ingenerio Juarez','Formosa Province, Argentina',  -23.0 - 47.0/60.0, - 61.0 - 48.0/60.0, 'America/Cordoba', 60))
-    studloc = a['Seattle']
-    daymins = []
-    dawns = []
-    dusks = []
-    moon = []
-    for row in range(len(timingData)):
-        entry = timingData.iloc[row,:]
-        day = entry.iloc[0].date()
-    
-        if pd.isnull(day): # handle the not-a-time missing date elements
-            daymins.append(np.NaN)
-            dawns.append(pd.NaT)
-            dusks.append(pd.NaT)
-            moon.append(np.NaN)
-            continue
-        
-        if entry['UID'].startswith('toba'):
-            sundata =  tobaloc.sun(date=day)
-        else:
-            sundata =  studloc.sun(date=day)
-        dt = sundata['sunrise']-sundata['sunset']
-        daymins.append( dt.seconds/60.0 )
-        dawns.append(sundata['sunrise'])
-        dusks.append(sundata['sunset'])
-        moon.append(a.moon_phase(day))
-    
-    timingData['Daylight minutes'] = daymins
-    timingData['Sunrise'] = dawns
-    timingData['Sunset'] = dusks
-    timingData['Moon phase'] = moon
-    
-    return timingData
-
-
-def firstAndLastActivity(data, threshold_list, resample=False):
-    ids = data.UID.unique()
-    firstlight = []
-    lastlight = []
-    min2fl = []
-    min2ll = []
-    timabove = []
-    whoswatch = []
-    watchperiod = []
-    thresholds = []
-
-    for id in ids:
-            print(id)
-            these_rows = (data.UID == id) & (data['Interval Status'] == 'ACTIVE')
-            daysofdata = set( data[ these_rows ].index.date )
-            for a_day in daysofdata:
-                nextday = a_day + pd.tseries.offsets.Day()
-                nextday = nextday.date().isoformat()
-                thisday = a_day.isoformat()
-                daylight = data['Activity'][these_rows][thisday + ' 04:00:00' : nextday + ' 03:59:00']
-                if resamp:
-                    daylight = daylight.resample(resamp[1]).apply(resamp[0]) 
-                for a_thresh in threshold_list:                                  
-                    thresholds.append(a_thresh)
-                    if a_thresh==0:
-                        abovethresh = daylight.index[ daylight < a_thresh+3]
-                    else:
-                        abovethresh = daylight.index[ daylight > a_thresh]
-                    if len(daylight)>15: # let's make sure we have at least 15min of daytime to work with
-                        # sometimes the index skips a few minutes here and there... this might be due to the Interval Status
-                        # so we need a way to find the true period in this noisy data...  
-                        dperiod = pd.Timedelta(np.median(daylight.index[1:15]-daylight.index[0:14])) # this is the update rate of the watch
-                        timeabove = len(abovethresh) * dperiod 
-                    else:
-                        dperiod = 0
-                        timeabove = pd.Timedelta('0 seconds')                        
-                    timabove.append(timeabove)
-                    watchperiod.append(dperiod)
-                    try:
-                        timelight = abovethresh[-1] # last time is above threshold
-                        mins4am = (timelight.time().hour - 4) * 60 + timelight.time().minute
-                        if mins4am<0: # if after midnight, then value above is negative
-                            mins4am += 24*60 # fix by adding 24 hours to it
-                    except IndexError: # there is no above threshold level all day long
-                        timelight = np.nan
-                        mins4am = np.nan
-                    lastlight.append(timelight)
-                    min2ll.append(mins4am)
-                    try:
-                        timelight = abovethresh[0] # first time is above threshold
-                        mins4am = (timelight.time().hour - 4) * 60 + timelight.time().minute
-                        if mins4am<0: # if after midnight, then value above is negative
-                            mins4am += 24*60 # fix by adding 24 hours to it
-                    except IndexError: # there is no above threshold level all day long
-                        timelight = np.nan
-                        mins4am = np.nan
-                    firstlight.append(timelight)
-                    min2fl.append(mins4am)
-                    whoswatch.append(id)
-                    #print("{} {} {} of 0-lux with period {}\n".format(id,a_day,len(daylight[daylight==0])*dperiod,dperiod))
-                
-                #print( len( whoswatch), len(lastlight), len(firstlight), len(min2ll), len(min2fl))
-    return pd.DataFrame( {'UID': whoswatch, 'Threshold': thresholds,
-                          'Last Activity': lastlight, 'Mins to LA from 4AM': min2ll,
-                          'First Activity': firstlight, 'Mins to FA from 4AM': min2fl,
-                          'Time above threshold': timabove, 
-                          'Minutes above threshold': [ el.total_seconds()/60.0 for el in timabove],
-                          'Watch period': watchperiod 
-                         } )
-
